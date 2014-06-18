c309a92ad84cd6c646c7fe06e96881146a4422f7
kozi <martin@kozianka.de>
2014-06-18 09:44:42 +0200

    - Removed debug output

--- src/modules/SimpletippMatches.php
+++ src/modules/SimpletippMatches.php
@@ -35,9 +35,6 @@ class SimpletippMatches extends SimpletippModule {
         // So maybe the current result is not the final one
         $this->finishedMatches();
 
-        $this->import('SimpletippEmailReminder');
-        $this->SimpletippEmailReminder->tippReminder();
-
 		if (TL_MODE == 'BE') {
 			$this->Template = new \BackendTemplate('be_wildcard');
 			$this->Template->wildcard  = '### SimpletippMatches ###';
