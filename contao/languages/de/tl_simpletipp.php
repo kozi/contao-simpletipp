a4aac5c2ff1869295d0cb538a239ff309f5a3f72
Martin Kozianka <martin@kozianka.de>
2015-07-14 10:52:15 +0200

    Added require-dev

--- contao/languages/de/tl_simpletipp.php
+++ contao/languages/de/tl_simpletipp.php
@@ -1,57 +1,47 @@
 <?php
 
-
-$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_legend']            = 'Wettbewerb';
-$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_legend_questions']  = 'Extrafragen';
-$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_pokal_legend']      = 'Pokalrunden';
-$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_matches']           = 'Spiele';
-
-// Global Button
-$GLOBALS['TL_LANG']['tl_simpletipp']['reminder']     = ['Email-Erinnerungen', 'Email-Erinnerungen abschicken'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_legend']           = 'Wettbewerb';
+$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_legend_questions'] = 'Extrafragen';
+$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_pokal_legend']     = 'Pokalrunden';
+$GLOBALS['TL_LANG']['tl_simpletipp']['simpletipp_matches']          = 'Spiele';
 
 
-// Fields
-$GLOBALS['TL_LANG']['tl_simpletipp']['title']        = ['Titel', 'Titel für diese Tipprunde'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['adminName']    = ['Admin Name','Name des Tipprundenadmins'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['adminEmail']   = ['Admin Email','Email des Tipprundenadmins'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['matchLength']  = ['Spiellänge','Länge eines Spiel in Sekunden (Beispiel: 2*45[Min] + 15[Min] = 6300 [Sekunden])'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['factor']       = ['Punkteverteilung', 'Punkteverteilung (Richtig, Differenz, Tendenz)'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['leagueID']     = ['Liga', 'Liga die getippt werden soll.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['leagueInfos']  = ['LigaInfos', 'LigaInfos'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['teaser']       = ['Info', 'Info'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['title']             = ['Titel', 'Titel für diese Tipprunde'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['adminName']         = ['Admin Name','Name des Tipprundenadmins'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['adminEmail']        = ['Admin Email','Email des Tipprundenadmins'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['matchLength']       = ['Spiellänge','Länge eines Spiel in Sekunden (Beispiel: 2*45[Min] + 15[Min] = 6300 [Sekunden])'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['factor']            = ['Punkteverteilung', 'Punkteverteilung (Richtig, Differenz, Tendenz)'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['leagueID']          = ['Liga', 'Liga die getippt werden soll.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['leagueInfos']       = ['LigaInfos', 'LigaInfos'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['teaser']            = ['Info', 'Info'];
 
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_ranges']      = ['Pokal Spieltage', 'Es müssen 6 sich nicht überschneidende Abschnitte gewählt werden.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_group']       = ['Gruppenphase', 'Spieltage festlegen.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_16']          = ['1. Runde', 'Spieltage festlegen.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_8']           = ['Achtelfinale', 'Spieltage festlegen.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_4']           = ['Viertelfinale', 'Spieltage festlegen.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_2']           = ['Halbfinale', 'Spieltage festlegen.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_finale']      = ['Finale', 'Spieltage festlegen.'];
 
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_ranges'] = ['Pokal Spieltage', 'Es müssen 6 sich nicht überschneidende Abschnitte gewählt werden.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_group']  = ['Gruppenphase', 'Spieltage festlegen.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_16']     = ['1. Runde', 'Spieltage festlegen.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_8']      = ['Achtelfinale', 'Spieltage festlegen.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_4']      = ['Viertelfinale', 'Spieltage festlegen.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_2']      = ['Halbfinale', 'Spieltage festlegen.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['pokal_finale'] = ['Finale', 'Spieltage festlegen.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['matchgroup']        = ['Gruppe', 'Gruppe/Spieltag an dem die Spiele stattfinden.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['competition']       = ['Wettbewerb', 'Wettbewerb'];
 
+$GLOBALS['TL_LANG']['tl_simpletipp']['tstamp']            = ['Geändert am', 'Geändert am'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['matches']           = ['Spiele', 'Spiele'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['match']             = ['Spiel', 'Spiel'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['results']           = ['Ergebnisse', 'Ergebnisse'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['result']            = ['Ergebnis', 'Ergebnis'];
 
+$GLOBALS['TL_LANG']['tl_simpletipp']['participant_group'] = ['Mitgliedergruppe', 'Auswahl einer Mitgliedergruppe.'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['participants']      = ['Teilnehmer', 'Teilnehmer'];
 
-$GLOBALS['TL_LANG']['tl_simpletipp']['matchgroup']   = ['Gruppe', 'Gruppe/Spieltag an dem die Spiele stattfinden.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['competition']  = ['Wettbewerb', 'Wettbewerb'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['new']               = ['Neue Tipprunde', 'Eine neue Tipprunde erstellen'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['reminder']          = ['Email-Erinnerungen', 'Email-Erinnerungen abschicken'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['edit']              = ['Tipprunde bearbeiten', 'Tipprunde ID %s bearbeiten'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['delete']            = ['Tipprunde löschen', 'Tipprunde ID %s löschen'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['toggle']            = ['Tipprunde veröffentlichen/unveröffentlichen', 'Tipprunde ID %s veröffentlichen/unveröffentlichen'];
 
-$GLOBALS['TL_LANG']['tl_simpletipp']['tstamp']       = ['Geändert am', 'Geändert am'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['matches']      = ['Spiele', 'Spiele'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['match']        = ['Spiel', 'Spiel'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['results']      = ['Ergebnisse', 'Ergebnisse'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['result']       = ['Ergebnis', 'Ergebnis'];
-
-
-$GLOBALS['TL_LANG']['tl_simpletipp']['participant_group']  = ['Mitgliedergruppe', 'Auswahl einer Mitgliedergruppe.'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['participants']       = ['Teilnehmer', 'Teilnehmer'];
-
-$GLOBALS['TL_LANG']['tl_simpletipp']['manage_questions'] = ['Fragen verwalten', 'Fragen verwalten'];
-
-$GLOBALS['TL_LANG']['tl_simpletipp']['new']      = ['Neue Tipprunde', 'Eine neue Tipprunde erstellen'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['edit']     = ['Bearbeiten', 'Bearbeiten'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['delete']   = ['Löschen', 'Löschen'];
-
-
-
-$GLOBALS['TL_LANG']['tl_simpletipp']['edit']       = ['Tipprunde bearbeiten', 'Tipprunde ID %s bearbeiten'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['delete']     = ['Tipprunde löschen', 'Tipprunde ID %s löschen'];
-$GLOBALS['TL_LANG']['tl_simpletipp']['toggle']     = ['Tipprunde veröffentlichen/unveröffentlichen', 'Tipprunde ID %s veröffentlichen/unveröffentlichen'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['calculate']         = ['Punkte neu berechnen', 'Punkte neu berechnen'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['update']            = ['Spieldaten aktualisieren', 'Spieldaten aktualisieren'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['pokal']             = ['Neue Pokalrunde auslosen', 'Neue Pokalrunde auslosen'];
+$GLOBALS['TL_LANG']['tl_simpletipp']['questions']         = ['Fragen verwalten', 'Fragen verwalten'];
