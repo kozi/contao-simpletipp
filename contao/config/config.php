e10bdf22701802925f289f8774446261a219d3f4
Martin Kozianka <martin@kozianka.de>
2015-07-20 21:18:23 +0200

    Bugfix and addition
    Small bugfix
    Added new bundesliga clubs

--- contao/config/config.php
+++ contao/config/config.php
@@ -43,72 +43,57 @@ array_insert($GLOBALS['TL_CTE']['simpletipp'], 0, [
     'simpletipp_statistics' => '\Simpletipp\Elements\ContentSimpletippStatistics',
 ]);
 
-array_insert($GLOBALS['BE_MOD'], 1, array(
-		'simpletipp' => array(
-				'simpletipp_group' => array
-				(
-						'tables'     => array('tl_simpletipp', 'tl_simpletipp_question'),
+array_insert($GLOBALS['BE_MOD'], 1, [
+		'simpletipp' => [
+				'simpletipp_group' => [
+						'tables'     => ['tl_simpletipp', 'tl_simpletipp_question'],
 						'icon'       => 'system/modules/simpletipp/assets/images/soccer.png',
 						'javascript' => 'system/modules/simpletipp/assets/simpletipp-backend.js',
 						'stylesheet' => 'system/modules/simpletipp/assets/simpletipp-backend.css',
-                        'update'     => array('\Simpletipp\SimpletippMatchUpdater', 'updateMatches'),
-                        'calculate'  => array('\Simpletipp\SimpletippMatchUpdater', 'calculateTipps'),
-                        'pokal'      => array('\Simpletipp\SimpletippPokal', 'calculate'),
-                        'reminder'   => array('\Simpletipp\SimpletippEmailReminder', 'tippReminder'),
-				),
-				'simpletipp_match' => array
-				(
-						'tables'     => array('tl_simpletipp_match'),
+                        'update'     => ['\Simpletipp\SimpletippMatchUpdater', 'updateMatches'],
+                        'calculate'  => ['\Simpletipp\SimpletippMatchUpdater', 'calculateTipps'],
+                        'pokal'      => ['\Simpletipp\SimpletippPokal', 'calculate'],
+                        'reminder'   => ['\Simpletipp\SimpletippEmailReminder', 'tippReminder'],
+				],
+				'simpletipp_match' => [
+						'tables'     => ['tl_simpletipp_match'],
 						'icon'       => 'system/themes/default/images/tablewizard.gif',
                         'stylesheet' => 'system/modules/simpletipp/assets/simpletipp-backend.css',
-				),
-				'simpletipp_tipp'   => array
-				(
-						'tables'     => array('tl_simpletipp_tipp'),
+				],
+				'simpletipp_tipp'   => [
+						'tables'     => ['tl_simpletipp_tipp'],
 						'icon'       => 'system/themes/default/images/tablewizard.gif',
                         'stylesheet' => 'system/modules/simpletipp/assets/simpletipp-backend.css',
-				)
+				]
 				
-		)
-));
-
-
-// leagueID 676 WM-2014
-$GLOBALS['simpletipp']['groupNames'][676] = [
-    'Gruppe A' => array('Brasilien', 'Kroatien', 'Mexiko', 'Kamerun'),
-    'Gruppe B' => array('Spanien', 'Niederlande', 'Chile', 'Australien'),
-    'Gruppe C' => array('Kolumbien', 'Griechenland', 'Elfenbeinküste', 'Japan'),
-    'Gruppe D' => array('Uruguay', 'Costa Rica', 'England', 'Italien'),
-    'Gruppe E' => array('Schweiz', 'Ecuador', 'Frankreich', 'Honduras'),
-    'Gruppe F' => array('Argentinien', 'Bosnien-Herz.', 'Iran', 'Nigeria'),
-    'Gruppe G' => array('Deutschland', 'Portugal', 'Ghana', 'USA'),
-    'Gruppe H' => array('Belgien', 'Algerien', 'Russland', 'Südkorea'),
-];
-
+		]
+]);
 
 $GLOBALS['simpletipp']['teamShortener'] = [
-    'Borussia Dortmund'            => array('Dortmund', 'BVB', 'vereinslogos/dortmund.png'),
-    'Bayern München'               => array('Bayern', 'FCB', 'vereinslogos/bayern.png'),
-    'Hamburger SV'                 => array('HSV', 'HSV', 'vereinslogos/hamburg.png'),
-    'SC Freiburg'                  => array('Freiburg', 'SCF', 'vereinslogos/freiburg.png'),
-    'FC Schalke 04'                => array('Schalke', 'S04', 'vereinslogos/schalke.png'),
-    'Borussia Mönchengladbach'     => array('Gladbach', 'GLA', 'vereinslogos/gladbach.png'),
-    'Bayer 04 Leverkusen'          => array('Leverkusen', 'LEV', 'vereinslogos/leverkusen.png'),
-    'Hannover 96'                  => array('Hannover', 'H96', 'vereinslogos/hannover.png'),
-    'VfL Wolfsburg'                => array('Wolfsburg', 'WOL', 'vereinslogos/wolfsburg.png'),
-    'TSG 1899 Hoffenheim'          => array('Hoffenheim', 'HOF', 'vereinslogos/hoffenheim.png'),
-    '1. FC Nürnberg'               => array('Nürnberg', 'FCN', 'vereinslogos/nuernberg.png'),
-    '1. FSV Mainz 05'              => array('Mainz', 'MAI', 'vereinslogos/mainz.png'),
-    'VfB Stuttgart'                => array('Stuttgart', 'VFB', 'vereinslogos/stuttgart.png'),
-    'FC Augsburg'                  => array('Augsburg', 'FCA', 'vereinslogos/augsburg.png'),
-    'Eintracht Braunschweig'       => array('Braunschweig', 'BRA', 'vereinslogos/braunschweig.png'),
-    'Werder Bremen'                => array('Werder', 'BRE', 'vereinslogos/werder.png'),
-    'Hertha BSC'                   => array('Hertha', 'BSC', 'vereinslogos/hertha.png'),
-    'Eintracht Frankfurt'          => array('Frankfurt', 'FRA', 'vereinslogos/frankfurt.png'),
-    'Fortuna Düsseldorf'           => array('Düsseldorf', 'DUS', 'vereinslogos/duesseldorf.png'),
-    'SpVgg Greuther Fuerth'        => array('Fürth', 'FUE', 'vereinslogos/fuerth.png'),
-    'SC Paderborn 07'              => array('Paderborn','SCP', 'vereinslogos/paderborn.png'),
-    '1. FC Köln'                   => array('Köln','1FC', 'vereinslogos/koeln.png'),
+    'Borussia Dortmund'            => ['Dortmund', 'BVB', 'vereinslogos/dortmund.png'],
+    'Bayern München'               => ['Bayern', 'FCB', 'vereinslogos/bayern.png'],
+    'Hamburger SV'                 => ['HSV', 'HSV', 'vereinslogos/hamburg.png'],
+    'SC Freiburg'                  => ['Freiburg', 'SCF', 'vereinslogos/freiburg.png'],
+    'FC Schalke 04'                => ['Schalke', 'S04', 'vereinslogos/schalke.png'],
+    'Borussia Mönchengladbach'     => ['Gladbach', 'GLA', 'vereinslogos/gladbach.png'],
+    'Bayer 04 Leverkusen'          => ['Leverkusen', 'LEV', 'vereinslogos/leverkusen.png'],
+    'Hannover 96'                  => ['Hannover', 'H96', 'vereinslogos/hannover.png'],
+    'VfL Wolfsburg'                => ['Wolfsburg', 'WOL', 'vereinslogos/wolfsburg.png'],
+    'TSG 1899 Hoffenheim'          => ['Hoffenheim', 'HOF', 'vereinslogos/hoffenheim.png'],
+    '1. FC Nürnberg'               => ['Nürnberg', 'FCN', 'vereinslogos/nuernberg.png'],
+    '1. FSV Mainz 05'              => ['Mainz', 'MAI', 'vereinslogos/mainz.png'],
+    'VfB Stuttgart'                => ['Stuttgart', 'VFB', 'vereinslogos/stuttgart.png'],
+    'FC Augsburg'                  => ['Augsburg', 'FCA', 'vereinslogos/augsburg.png'],
+    'Eintracht Braunschweig'       => ['Braunschweig', 'BRA', 'vereinslogos/braunschweig.png'],
+    'Werder Bremen'                => ['Werder', 'BRE', 'vereinslogos/werder.png'],
+    'Hertha BSC'                   => ['Hertha', 'BSC', 'vereinslogos/hertha.png'],
+    'Eintracht Frankfurt'          => ['Frankfurt', 'FRA', 'vereinslogos/frankfurt.png'],
+    'Fortuna Düsseldorf'           => ['Düsseldorf', 'DUS', 'vereinslogos/duesseldorf.png'],
+    'SpVgg Greuther Fuerth'        => ['Fürth', 'FUE', 'vereinslogos/fuerth.png'],
+    'SC Paderborn 07'              => ['Paderborn','SCP', 'vereinslogos/paderborn.png'],
+    '1. FC Köln'                   => ['Köln','1FC', 'vereinslogos/koeln.png'],
+    'FC Ingolstadt 04'             => ['Ingolstadt','FCI', 'vereinslogos/ingolstadt.png'],
+    'SV Darmstadt 98'              => ['Darmstadt','SCD', 'vereinslogos/darmstadt.png'],
 ];
 
 
--- src/Simpletipp/SimpletippMatchUpdater.php
+++ src/Simpletipp/SimpletippMatchUpdater.php
@@ -265,7 +265,7 @@ class SimpletippMatchUpdater extends \Backend {
                 'team_h_three'    => Simpletipp::teamShortener($tmp['nameTeam1'], true),
                 'team_a_three'    => Simpletipp::teamShortener($tmp['nameTeam2'], true),
                 'icon_h'          => (Simpletipp::teamIcon($tmp['nameTeam1'])) ? Simpletipp::teamIcon($tmp['nameTeam1']) : $tmp['iconUrlTeam1'],
-                'icon_a'          => (Simpletipp::teamIcon($tmp['nameTeam1'])) ? Simpletipp::teamIcon($tmp['nameTeam2']) : $tmp['iconUrlTeam2'],
+                'icon_a'          => (Simpletipp::teamIcon($tmp['nameTeam2'])) ? Simpletipp::teamIcon($tmp['nameTeam2']) : $tmp['iconUrlTeam2'],
 
                 'isFinished'      => $tmp['matchIsFinished'],
                 'lastUpdate'      => strtotime($tmp['lastUpdate']),
