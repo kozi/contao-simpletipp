a23cdf843433bc058c92b78b28baf0575d4f6d3c
kozi <martin@kozianka.de>
2016-10-19 16:44:33 +0200

    More telegram implemetations

--- src/Simpletipp/Modules/SimpletippTelegram.php
+++ src/Simpletipp/Modules/SimpletippTelegram.php
@@ -58,6 +58,7 @@ class SimpletippTelegram extends SimpletippModule
     protected function compile()
     {
         $this->commander = new TelegramCommander($this->simpletipp_telegram_bot_key);
+        $this->chatMember = $this->commander->getChatMember();
 
         $this->text = $this->commander->getText();
         if ($this->text === null) {
@@ -69,35 +70,40 @@ class SimpletippTelegram extends SimpletippModule
             // Handle start command
             $this->handleStart();
         }
-        elseif ($this->commander->getChatMember() === null) {
+        elseif ($this->chatMember === null) {
             $this->commander->sendText('Chat not registered.');            
             exit;
         }
 
         $t = strtolower($this->text);
         switch ($t) {
+            case "/h":
             case "h":
                 $this->showHighscore();
                 break;
+            case "/hd":
+            case "hd":
+                $this->showHighscore(true);
+                break;
+            case "/t":
             case "t":
                 $this->handleTipp(true);
                 break;
+            case "/s":                
             case "s":
                 $this->showSpiele();
                 break;
-            case "z":
+            case "/z":
+            case "z":            
                 $this->showZeigler();
-                break; 
+                break;
+            case "/c":                 
             case "c":
                 $this->showZitat();
                 break;               
             default:
-                if(false) { // TODO Check if match_id is correct and "fresh"
-                    $this->handleTipp();
-                }
-                else {
-                    // Do something funny!
-                    // Foto, Zitat, Sticker... 
+                if(!$this->handleTipp()) {
+                    $this->showHelp();
                 }
         }
         exit;
@@ -105,16 +111,86 @@ class SimpletippTelegram extends SimpletippModule
 
     private function handleTipp($isInitial = false) {
         $this->commander->chatAction(Actions::TYPING);
-        
-        // Trage einen Tipp ein und zeige das nächste Spiel
+        $newMatchQuestion = false;
+        $stackFile = TL_ROOT."/system/tmp/TELEGRAM_".$this->simpletipp_telegram_url_token.$this->chatMember->id.".spc";
+        $stack     = null;
+        // Den Stack holen
+        if (!$isInitial && file_exists($stackFile)) {
+            $stack = unserialize(file_get_contents($stackFile));
+        }
+        if ($isInitial) {
+            $this->commander->sendText("Format: `[HEIM]:[AUSWÄRTS]`\nBeispiele: `2:0`, `1:1`, `5:0`\nSchicke einen `-` zum Überspringen oder einen `.` zum Beenden.");
+            $stack = (object) [
+                "lastAccess" => time(),
+                "tipps"      => []
+            ];
+        }
+        if ($stack === null || ($stack->lastAccess + 120) < time()) {
+            return false;
+        }
+
+        // Tippen beenden --> stackFile löschen
+        if ("." === $this->text) {
+            unlink($stackFile);
+            $this->commander->sendText("Tippen beendet!");
+            return true;
+        }
+
+        $excludeIds = array_keys($stack->tipps);
+
+        // Infos zur aktuellesten ID auf dem stack holen und an den Client schicken!
+		$sql  = "SELECT id,title,team_h,team_a,deadline FROM tl_simpletipp_match WHERE leagueID = ? AND deadline > ?";
+        $sql .= (is_array($excludeIds) && count($excludeIds) > 0) ? " AND id NOT IN (".implode($excludeIds, ",").")" : "";
+        $sql .= " ORDER BY deadline ASC";
+
+        $result = $this->Database->prepare($sql)->limit(1)->execute($this->simpletipp->leagueID, $this->now);
+        if ($result->numRows == 1) {
+            $match = (Object) $result->row();
+            $match->teamHome = SimpletippTeamModel::findByPk($match->team_h);
+            $match->teamAway = SimpletippTeamModel::findByPk($match->team_a);
+
+		    $sql = "SELECT * FROM tl_simpletipp_tipp WHERE member_id = ? AND match_id = ?";  
+            $result = $this->Database->prepare($sql)->limit(1)->execute($this->chatMember->id, $match->id);
+            if ($result->numRows == 1) {
+                $match->tipp = $result->tipp;
+            }
+            
+            $stack->tipps[$match->id] = $match;
+
+            $newMatchQuestion = sprintf("*%s*\n%s",
+                $match->teamHome->short." - ".$match->teamAway->short,
+                ($match->tipp) ? "Bisheriger Tipp: `".$match->tipp."`" : "" 
+            );
+        }
+
+        // Tipp eintragen
+        if (!$isInitial && is_array($stack->tipps) && count($stack->tipps) > 0 && $this->text !== "-") {
+            // - Bedeutet aktuellen Tipp beibehalten bzw. das Spiel nicht tippen
+            // TODO $this->text auswerten!
+            $this->commander->sendText("`TIPP: ".$this->text."`");
+            // TODO Wenn es nicht klappt die letzte id vom stack holen
+            // Tippformatbeispiel: *2:1* (*.* zum Beenden. *-* zum Überspringen)
+            // Die letzte ID vom stack holen damit das Spiel nochmal getippt werden kann
+        }
+
+
+        if ($newMatchQuestion !== false) {
+            $this->commander->sendText($newMatchQuestion);
+        }
+
+        // Den stack speichern
+        $stack->lastAccess = time();
+        file_put_contents($stackFile, serialize($stack));
+
+        return true;
     }
 
-    private function showHighscore() {
+    private function showHighscore($details = false) {
         $this->commander->chatAction(Actions::TYPING);
         // Zeige den Highscore
         $highscore = $this->getHighscore();
-        $result    = "<pre>";
-        $padding   = ['index' => 0, 'username' => 0, 'points' => 0, 'sum_perfect' => 0, 'sum_difference' => 0, 'sum_tendency' => 0];
+        $result    = "";
+        $padding   = ['index' => 0, 'points' => 0, 'sum_perfect' => 0, 'sum_difference' => 0, 'sum_tendency' => 0];
         $i         = 1;
         $list      = [];
         $arrIcon   = [
@@ -123,15 +199,16 @@ class SimpletippTelegram extends SimpletippModule
                     
         foreach($highscore as $r) {
             $icon    = (array_key_exists($i, $arrIcon)) ? $arrIcon[$i] : " ";
-            $list[]  = [
-                $i++,
-                $r->username,
-                $r->points,
-                $r->sum_perfect,
-                $r->sum_difference,
-                $r->sum_tendency
+            $name    = $r->firstname." ".substr($r->lastname,0,1).".";
+            $list[]  = (Object) [
+                "index" => $i++,
+                "username" => $r->username,
+                "name" =>  $name,
+                "points" => $r->points,
+                "sum_perfect" => $r->sum_perfect,
+                "sum_difference" => $r->sum_difference,
+                "sum_tendency" => $r->sum_tendency
             ];
-            $padding['username'] = (strlen($r->username."") > $padding['username']) ? strlen($r->username."") : $padding['username'];
             $padding['points'] = (strlen($r->points."") > $padding['points']) ? strlen($r->points."") : $padding['points'];
             $padding['sum_perfect'] = (strlen($r->sum_perfect."") > $padding['sum_perfect']) ? strlen($r->sum_perfect."") : $padding['sum_perfect'];
             $padding['sum_difference'] = (strlen($r->sum_difference."") > $padding['sum_difference']) ? strlen($r->sum_difference."") : $padding['sum_difference'];
@@ -140,34 +217,51 @@ class SimpletippTelegram extends SimpletippModule
         $padding['index'] = strlen($i."");        
 
         foreach($list as $r) {
-            $result .= sprintf("%s. %s %s%s\n",
-                str_pad($r[0], $padding['index'], '0', STR_PAD_LEFT),
-                str_pad($r[1], $padding['username'], ' ', STR_PAD_RIGHT),
-                str_pad($r[2], $padding['points'], ' ', STR_PAD_LEFT),
-                /* [%s,%s,%s] ---  str_pad($r[3], $padding['sum_perfect'], ' ', STR_PAD_LEFT),
-                str_pad($r[4], $padding['sum_difference'], ' ', STR_PAD_LEFT),
-                str_pad($r[5], $padding['sum_tendency'], ' ', STR_PAD_LEFT)*/
-                ($this->commander->getChatMember()->username == $r[1]) ? " \xF0\x9F\x99\x88" : ""
-            );
+            $userCommand = "/".str_replace(".", "\_", $r->username);
+            if ($details) {
+                $result .= sprintf("`%s. %s[%s,%s,%s]` %s\n",
+                    str_pad($r->index, $padding['index'], '0', STR_PAD_LEFT),
+                    str_pad($r->points, $padding['points'], ' ', STR_PAD_LEFT),
+                    str_pad($r->sum_perfect, $padding['sum_perfect'], ' ', STR_PAD_LEFT),
+                    str_pad($r->sum_difference, $padding['sum_difference'], ' ', STR_PAD_LEFT),
+                    str_pad($r->sum_tendency, $padding['sum_tendency'], ' ', STR_PAD_LEFT),
+                    ($this->chatMember->username == $r->username) ? "*".$userCommand."*" : $userCommand
+                    // ($this->commander->getChatMember()->username == $r->username) ? "\xF0\x9F\x99\x88" : "\xF0\x9F\x99\x88"
+                );
+            } else {
+                $result .= sprintf("`%s.[%s]%s` %s %s\n",
+                    str_pad($r->index, $padding['index'], '0', STR_PAD_LEFT),
+                    str_pad($r->points, $padding['points'], ' ', STR_PAD_LEFT),
+                    ($this->chatMember->username == $r->username) ? "*" : "",
+                    ($this->chatMember->username == $r->username) ? "*".$r->name."*" : $r->name,
+                    $userCommand 
+                    // ($this->chatMember->username == $r->username) ? "\xF0\x9F\x99\x88" : "\xF0\x9F\x99\x88"
+                );
+            }
         }
-        $return = $this->commander->sendText($result."</pre>", "HTML");
+        $return = $this->commander->sendText($result); // , "HTML"
         $this->commander->sendText(json_encode($return));
     }
 
     private function showSpiele() {
-        $userId   = $this->commander->getChatMember()->id;
+
+        // TODO Titel anzeigen!
+        // TODO Spiele von anderen Benutzern anzeigen
+
+        $this->commander->chatAction(Actions::TYPING);
         $ball     = "\xE2\x9A\xBD";
         $groupIds = [];
-        $this->commander->chatAction(Actions::TYPING);
-        $this->import("Database");
-
-        $result = $this->Database->prepare("SELECT groupID FROM tl_simpletipp_match WHERE leagueID = ? AND deadline < ? ORDER BY deadline DESC")->limit(1)->execute($this->simpletipp->leagueID, $this->now);
+        $result   = $this->Database->prepare("SELECT groupID FROM tl_simpletipp_match
+             WHERE leagueID = ? AND deadline < ? ORDER BY deadline DESC")
+             ->limit(1)->execute($this->simpletipp->leagueID, $this->now);
         if ($result->numRows == 1) {
             $groupIds[] = $result->groupID;
         }
 
-        $result = $this->Database->prepare("SELECT groupID FROM tl_simpletipp_match WHERE leagueID = ? AND deadline > ? ORDER BY deadline ASC")->limit(1)->execute($this->simpletipp->leagueID, $this->now);
-        if ($result->numRows == 1) {
+        $result = $this->Database->prepare("SELECT groupID FROM tl_simpletipp_match
+             WHERE leagueID = ? AND deadline > ? ORDER BY deadline ASC")
+             ->limit(1)->execute($this->simpletipp->leagueID, $this->now);
+        if ($result->numRows == 1 && count($groupIds) === 0) {
             $groupIds[] = $result->groupID;
         }
 
@@ -183,8 +277,8 @@ class SimpletippTelegram extends SimpletippModule
 		 	AND (tipps.member_id = ? OR tipps.member_id IS NULL)
             AND matches.groupID IN (".implode($groupIds,",").") ORDER BY deadline ASC";
         
-        $content = "<pre>";
-        $result  = $this->Database->prepare($sql)->execute($userId, $this->simpletipp->leagueID, $userId);
+        $content = "";
+        $result  = $this->Database->prepare($sql)->execute($this->chatMember->id, $this->simpletipp->leagueID, $this->chatMember->id);
 
         while ($result->next()) {
             $match = (Object) $result->row();
@@ -192,26 +286,33 @@ class SimpletippTelegram extends SimpletippModule
             $match->teamAway = SimpletippTeamModel::findByPk($match->team_a);
             $pointObj      = new SimpletippPoints($this->pointFactors, $match->perfect, $match->difference, $match->tendency);
 			$match->points = $pointObj->points;
-            $content .= sprintf("%s-%s %s (%s) ",
-                $match->teamHome->three,
-                $match->teamAway->three,
+
+            
+
+            //$match->teamHome->short,
+            //$match->teamAway->short,
+
+            $content .= sprintf("`%s - %s`\n`%s [%s]` %s /%s\_%s\n",
+                $match->teamHome->short,
+                $match->teamAway->short,
                 (strlen($match->tipp) > 2) ? $match->tipp : "?:?",
-                (strlen($match->result) > 2) ? $match->result : "?:?"
+                (strlen($match->result) > 2) ? $match->result : "?:?",
+                str_repeat($ball, $match->points).str_repeat("\xE2\x9A\xAA", 3-$match->points),
+                strtolower($match->teamHome->three),
+                strtolower($match->teamAway->three)               
             );
-            $content .= str_repeat($ball, $match->points)."\n";
-        }            
-        
-        $return = $this->commander->sendText($content."</pre>", "HTML");
+        }
+
+        $return = $this->commander->sendText($content);
     }
 
     private function handleStart() {
         $this->commander->chatAction(Actions::TYPING);
 
         // Chat schon registriert?
-        if($this->commander->getChatMember() !== null) {
-            $objMember = $this->commander->getChatMember();
+        if($this->chatMember !== null) {
             $tmpl = 'Chat already registered for %s (%s).';
-            $this->commander->sendText(sprintf($tmpl, $objMember->firstname.' '.$objMember->lastname, $objMember->username));
+            $this->commander->sendText(sprintf($tmpl, $this->chatMember->firstname.' '.$this->chatMember->lastname, $this->chatMember->username));
             return true;
         }
 
@@ -233,7 +334,7 @@ class SimpletippTelegram extends SimpletippModule
 
         $tmpl = 'Chat registered for %s (%s).';
         $this->commander->sendText(sprintf($tmpl, $objMember->firstname.' '.$objMember->lastname, $objMember->username));
-        $this->commander->sendInfoMessage();
+        $this->showHelp();
         return true;
     }
     
@@ -261,6 +362,11 @@ class SimpletippTelegram extends SimpletippModule
             return true;
         }
         return false;
+    }
+    
+    private function showHelp() {
+        $this->commander->sendText("Hilfe anzeigen!");
+        return true;
     }
 
     private function showZitat() {
