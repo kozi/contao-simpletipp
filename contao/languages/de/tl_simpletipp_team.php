98afbbd2b7d08363af28fae51d482f6452251ce7
Martin Kozianka <kozianka@otris.de>
2017-08-15 12:42:17 +0200

    Translation for team table

---
+++ contao/languages/de/tl_simpletipp_team.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_simpletipp_team']['name']     = ['Name', 'Name des Teams'];
+$GLOBALS['TL_LANG']['tl_simpletipp_team']['short']    = ['Kurzname', 'Abkürzung für den Teamnamen'];
+$GLOBALS['TL_LANG']['tl_simpletipp_team']['alias']    = ['Alias', 'Alias in Kleinschreibung und ohne Sonder- und Leerzeichen'];
+$GLOBALS['TL_LANG']['tl_simpletipp_team']['three']    = ['Abkürzung (3 Buchstaben)', 'Abkürzung mit genau drei Buchstaben'];
+$GLOBALS['TL_LANG']['tl_simpletipp_team']['logo']     = ['Logo', 'Logo des Vereins'];
