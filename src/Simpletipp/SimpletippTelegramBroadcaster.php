379863be9c193d3c8e919bbf37c998409dd4076e
Martin Kozianka <kozianka@otris.de>
2018-10-11 15:52:55 +0200

    Implementation update

--- src/Simpletipp/SimpletippTelegramBroadcaster.php
+++ src/Simpletipp/SimpletippTelegramBroadcaster.php
@@ -7,6 +7,8 @@ use Contao\ModuleModel;
 use Contao\System;
 use Simpletipp\Models\SimpletippMatchModel;
 use Simpletipp\Models\SimpletippModel;
+use Simpletipp\Models\SimpletippTippModel;
+use Telegram\Bot\Api;
 
 /**
  * Class SimpletippTelegramBroadcaster
@@ -18,41 +20,61 @@ use Simpletipp\Models\SimpletippModel;
  */
 class SimpletippTelegramBroadcaster extends Backend
 {
-    private $botKey = null;
+    private $telegram = null;
     private $match = null;
 
     public function __construct()
     {
-        $module = ModuleModel::findOneBy(['tl_module.simpletipp_telegram_bot_key <> ?'], '');
-        if ($module !== null) {
-            $this->botKey = $module->simpletipp_telegram_bot_key;
-            $this->match = SimpletippMatchModel::getNextMatch();
+        $moduleModel = ModuleModel::findOneBy(['tl_module.simpletipp_telegram_bot_key <> ?'], '');
+        $matchModel = SimpletippMatchModel::getNextMatch();
+        if ($moduleModel !== null && $matchModel !== null) {
+            $this->telegram = new Api($moduleModel->simpletipp_telegram_bot_key);
+            $this->match = $matchModel;
         }
         parent::__construct();
     }
 
     public function broadcastMessages()
     {
-        if ($this->botKey === null || $this->match === null) {
-            // No botKey found or no match found
+        if ($this->telegram === null || $this->match === null) {
+            // No bot key found or no match found
             return;
         }
 
-        $simpletippEntries = SimpletippModel::findBy(['tl_simpletipp.leagueID = ?'], $match->leagueID);
+        $hours = 3;
+        if (false && $this->match->deadline > (($hours * 3600) + time())) {
+            // match starts in more than $hours
+            return;
+        }
+
+        $simpletippEntries = SimpletippModel::findBy(['tl_simpletipp.leagueID = ?'], $this->match->leagueID);
         if ($simpletippEntries === null) {
             // match does not belong to a simpletipp entry
             return;
         }
+
         foreach ($simpletippEntries as $simpletipp) {
             $chatMember = $simpletipp->getGroupMember(true);
-            foreach ($chatMember as $member) {
-                broadcastToUser($member, $match);
+            foreach ($chatMember as $m) {
+                $this->broadcastToUser($m);
             }
         }
     }
 
     private function broadcastToUser($member)
     {
-        System::log(json_encode([$member->username, $this->match->row()]), 'SimpletippTelegramBroadcaster broadcastToUser()', 'TL_INFO');
+        if ($member->username !== 'kozi') {
+            return;
+        }
+        $tipp = SimpletippTippModel::findOneBy(['member_id = ?', 'match_id = ?'], [$member->id, $this->match->id]);
+        if (false && $tipp !== null) {
+            // already tipped
+            return;
+        }
+        $chatId = $member->telegram_chat_id;
+        $text = "Das Spiel " . $this->match->title . " startet bald! /t";
+        $response = $this->telegram->sendMessage(['chat_id' => $chatId, 'text' => $text]);
+        System::log("Broadcast to " . $member->username, 'SimpletippTelegramBroadcaster broadcastToUser()', 'TL_INFO');
+
     }
 }
