2720a2b7dc80fe58d2b4cfaa6505f903479d783a
Martin Kozianka <kozianka@otris.de>
2019-02-13 09:23:54 +0100

    started to implements simple telegram reminder

--- src/Simpletipp/SimpletippEmailReminder.php
+++ src/Simpletipp/SimpletippEmailReminder.php
@@ -5,6 +5,7 @@ namespace Simpletipp;
 use Contao\Email;
 use Simpletipp\Models\SimpletippMatchModel;
 use Simpletipp\Models\SimpletippModel;
+use Telegram\Bot\Api;
 
 /**
  * Class SimpletippEmailReminder
@@ -21,11 +22,13 @@ class SimpletippEmailReminder extends \Backend
         \System::loadLanguageFile('default');
 
         $this->isDebug = isset($_GET["debug"]);
+        $telegram = null;
         $simpletippModel = SimpletippModel::findAll();
         $hours = 24;
         $now = time();
         $arrMessages = [];
         foreach ($simpletippModel as $simpletippObj) {
+            $telegram = (strlen($simpletippObj->telegram_bot_key) > 0) ? new Api($simpletippObj->telegram_bot_key) : null;
             $match = SimpletippMatchModel::getNextMatch($simpletippObj->leagueID);
 
             if ($match == null) {
@@ -40,45 +43,67 @@ class SimpletippEmailReminder extends \Backend
                 $pageObj = \PageModel::findById($simpletippObj->matches_page);
                 $matchesPageUrl .= ($pageObj) ? $this->generateFrontendUrl($pageObj->row()) : '';
 
+                $teamH = $match->getRelated('team_h');
+                $teamA = $match->getRelated('team_a');
+                $matchDate = \Date::parse('d.m.Y H:i', $match->deadline);
+
                 $userNamesArr = [];
-                $emailSubject = sprintf($GLOBALS['TL_LANG']['simpletipp']['email_reminder_subject'],
-                    $match->title_short);
-                $emailText = sprintf($GLOBALS['TL_LANG']['simpletipp']['email_reminder_text'],
-                    $hours, $match->title, \Date::parse('d.m.Y H:i', $match->deadline), $matchesPageUrl);
+                $emailSubject = sprintf($GLOBALS['TL_LANG']['simpletipp']['reminder_email_subject'],
+                    $match->title_short . " (" . $matchDate . ")");
+                $emailText = sprintf($GLOBALS['TL_LANG']['simpletipp']['reminder_email_text'],
+                    $hours, $match->title, $matchDate, $matchesPageUrl);
+                $telegramText = sprintf($GLOBALS['TL_LANG']['simpletipp']['reminder_telegram_text'],
+                    $hours, $match->title, $matchDate, $matchesPageUrl);
 
-                $htmlLogos = '<img style="height:100px;" src="/' . $match->getRelated('team_h')->logoPath() . '"> '
-                . '<img style="height:100px;" src="/' . $match->getRelated('team_a')->logoPath() . '">';
+                $htmlLogos = "";
+                $commonStyle = "font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;";
 
-                $emailHtml = $this->textToHtml($emailText);
-                $emailText . replace("##LOGOS##", "");
-                $emailHtml . replace("##LOGOS##", $htmlLogos);
+                $teamH = $match->getRelated('team_h');
+                $teamA = $match->getRelated('team_a');
+                $htmlLogos .= \Image::getHtml(\Image::get($teamH->logoPath(), 100, 100, 'box'), $teamH->name, 'style="margin:20px 10px 20px 0;"');
+                $htmlLogos .= \Image::getHtml(\Image::get($teamA->logoPath(), 100, 100, 'box'), $teamA->name, 'style="margin:20px 0 20px 10px;"');
+
+                $emailHtml = '<div style="' . $commonStyle . '">' . str_replace("##LOGOS##", $htmlLogos, $this->textToHtml($emailText)) . '</div>';
+                $emailText = str_replace("##LOGOS##", "", $emailText);
+
                 if ($this->isDebug) {
-                    echo "<pre>match\n\n";
-                    var_dump($match->getRelated('team_h')->logoPath());
-                    var_dump($match->getRelated('team_a')->logoPath());
-                    echo "</pre><pre>emailSubject\n\n";
-                    echo $emailSubject;
-                    echo "\n\n\n</pre><pre>this->textToHtml(emailText)\n\n</pre><div style='border:1px solid #333;width:480px;padding:10px;'>";
-                    echo $emailHtml;
-                    echo "</div><pre>emailText\n\n";
-                    echo $emailText;
-                    echo "\n\n\n</pre>";
+                    echo '<div style="font-family:sans-serif;">';
+                    echo "<h2>emailSubject</h2>";
+                    echo "<pre style='border:1px solid #333;width:560px;padding:10px;'>" . $emailSubject . "</pre>";
+                    echo "<h2>emailHtml</h2>";
+                    echo "<div style='border:1px solid #333;width:560px;padding:10px;'>" . str_replace('src="assets', 'src="/assets', $emailHtml) . "</div>";
+                    echo "<h2>emailText</h2>";
+                    echo "<pre style='border:1px solid #333;width:560px;padding:10px;'>" . $emailText . "</pre>";
+                    echo "<h2>telegramText</h2>";
+                    echo "<pre style='border:1px solid #333;width:560px;padding:10px;'>" . $telegramText . "</pre>";
+
+                    $email = $this->generateEmailObject($simpletippObj, $emailSubject, $emailText, $emailHtml);
+                    $email->sendTo($simpletippObj->adminEmail);
                 }
 
                 $emailCount = 0;
                 foreach (static::getNotTippedUser($simpletippObj->participant_group, $match->id) as $u) {
+                    $telegramId = $u['telegram_chat_id'];
                     $emailSent = '';
                     if ($u['simpletipp_email_reminder'] == '1') {
                         $email = $this->generateEmailObject($simpletippObj, $emailSubject, $emailText, $emailHtml);
 
                         if ($this->isDebug) {
-                            $arrMessages[] = "EMAIL to: " . $u['email'] . " SUBJECT: " . $emailSubject;
+                            $arrMessages[] = $u['email'] . ", " . $emailSubject;
                         } else {
                             $email->sendTo($u['email']);
                         }
 
                         $emailSent = '@ ';
                         $emailCount++;
+                    }
+                    if ($telegram != null && $telegramId && strlen($telegramId) > 0 && "martin@kozianka.de" == $u['email']) {
+                        // var_dump([$u['email'], $telegramId]);
+
+                        $response = $telegram->sendMessage([
+                            "chat_id" => $telegramId,
+                            "text" => $telegramText,
+                        ]);
                     }
 
                     $userNamesArr[] = $emailSent . $u['firstname'] . ' ' . $u['lastname'] . ' (' . $u['username'] . ')';
@@ -100,9 +125,12 @@ class SimpletippEmailReminder extends \Backend
         } // END foreach
 
         if ($this->isDebug) {
-            echo "<pre>";
-            var_dump($arrMessages);
-            die("\n\n\nDEBUG DONE!</pre>");
+            echo "<ul>";
+            foreach ($arrMessages as $m) {
+                echo "<li><p>" . $m . "</p></li>";
+            }
+            echo "</ul>";
+            die("</div>");
         }
 
         if ('reminder' === \Input::get('key')) {
